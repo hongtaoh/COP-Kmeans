a66d553083a92cdbbb89158443badc71a7f6d041
hongtaoh <hongtaoh@cs.wisc.edu>
2024-10-29 16:29:15 -0500

    change pkg name

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='copkmeans-ht',
+    name='htcopkmeans',
     version='1.5',
     description='This is just a copy of the original repo https://github.com/Behrouz-Babaki/COP-Kmeans',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  
     author='Behrouz Babaki',
     author_email='',
     url='https://github.com/hongtaoh/COP-Kmeans',
